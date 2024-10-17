5eeb21237a135d726f8f4368e401700c4cde751f
PrincyChauhan <chauhanprincee7@gmail.com>
2024-10-17 14:17:21 +0530

    Env file created

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-const PORT = 8000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on ${PORT}`.white.bgMagenta)
 })
