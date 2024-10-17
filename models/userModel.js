8fc301e0919cdaabfebb493103bddf073fa90d3f
PrincyChauhan <chauhanprincee7@gmail.com>
2024-10-17 15:00:06 +0530

    Userschema added

---
+++ models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: [true, 'Please provide a username'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+    },
+    address: {
+        type: Array,
+
+    },
+    phone: {
+        type: Number,
+        required: [true, 'Please provide a phone number'],
+    },
+    usertype: {
+        type: String,
+        required: [true, 'Please provide a user type'],
+        default: 'client',
+        enum: ['client', 'admin', 'vendor', 'driver']
+    },
+    profile: {
+        type: String,
+        default: "https://www.flaticon.com/free-icon/user_149071"
+    },
+
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema);
